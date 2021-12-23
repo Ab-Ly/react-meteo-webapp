68e70ebae5fbddbafd2c1f83e5f6c1ace22a433a
Ab-Ly <abdelalilyoussefi@icloud.com>
2021-12-23 19:27:22 +0100

    addFooter

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>Created By React & Dev By @bLy</p>
+  </div>
+);
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import reactDom from "react-dom";
 import "./index.css";
 import App from "./App";
+import Footer from "./Components/Footer";
 
 reactDom.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById("root")
 );
